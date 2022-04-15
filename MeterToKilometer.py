9c4690bac42b89e82ff2149b74553ec090030339
Lavenderer1375 <86304384+Lavenderer1375@users.noreply.github.com>
2022-04-15 09:22:54 +0430

    Add files via upload

---
+++ MeterToKilometer.py
@@ -0,0 +1,8 @@
+def MtoKm():
+    Meter =int(float(input("Enter  Meter Unit to Change to Km:\n")))
+    Km = Meter / 1000
+    print (Km)
+
+user_input = ""
+while user_input != "Done":
+    MtoKm()
